2c6e4e78824f65ae8a17117ba9d16f4ab569cd00
Zevell <zevalgaming@gmail.com>
2021-09-20 10:35:39 +1000

    Delete help.js
    having both "commands" and "help" would have been confusing.

--- commands/help.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Help',
-  description: 'Shows relevant information about a command and how to use it.',
-  usage: 'help commandName (case insensitive)',
-  execute(message) {
-    // Code
-  },
-};
