0e95018c139c0ab325d3fe3c51407e3b124f222f
Zevell <zevalgaming@gmail.com>
2021-09-20 10:34:07 +1000

    Member info
    get information about a guild member, such as when they joined the guild (server), their nickname, etc.

--- commands/memberinfo.js
+++ commands/memberinfo.js
@@ -1,9 +1,50 @@
 module.exports = {
-  name: 'MemberInfo',
+  name: 'memberinfo',
   description:
     'Retrieves and displays information about a Discord guild member.',
-  usage: 'memberinfo @user',
-  execute(message) {
-    //code
+  usage: `memberinfo @user`,
+  execute: async (args) => {
+    if (!args.userArgs[0])
+      return args.message.reply(
+        `Please provide a user, usage: \n${this.usage}`
+      );
+
+    const member = await args.message.guild.members.fetch(
+      args.message.mentions.members.first().id
+    );
+
+    const joinedAt = member.joinedAt.toString().split(' ');
+    const embed = new args.Discord.MessageEmbed()
+      .setTitle(member.user.tag)
+      .setColor(args.color)
+      .addFields(
+        {
+          name: 'Name on Server',
+          value: member.displayName,
+          inline: true,
+        },
+        {
+          name: 'ID',
+          value: member.id,
+          inline: true,
+        },
+        {
+          name: 'Joined Date',
+          value: joinedAt.splice(0, 5).join(' '),
+          inline: false,
+        },
+        {
+          name: 'Temporary Member',
+          value: member.partial,
+          inline: true,
+        },
+        {
+          name: 'Top Role',
+          value: member.roles.highest,
+          inline: true,
+        }
+      );
+
+    args.message.channel.send(embed);
   },
 };
