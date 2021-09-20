a44cb064709759baa7aac2082c25af0c885eb01e
Zevell <zevalgaming@gmail.com>
2021-09-20 10:36:01 +1000

    Update eval.js
    more error handling, and use new args.

--- commands/eval.js
+++ commands/eval.js
@@ -1,34 +1,68 @@
+const buffer = require('buffer');
+
 module.exports = {
   name: 'eval',
   description: 'Evaluates the given code (bot dev only).',
   usage: 'No.',
-  execute: async (message, args, client) => {
-    if (message.author.id !== '105640584470937600') {
-      return message.reply('Hey! ||No!||');
+  execute: async (args) => {
+    let result;
+
+    await process.on('uncaughtException', function (err) {
+      if (args.message) result = err;
+      console.error(err);
+      console.log('Node NOT Exiting...');
+    });
+
+    if (args.message.author.id !== '105640584470937600') {
+      return args.message.reply('Hey! ||No!||');
     }
 
     try {
-      const code = message.content.split('eval ')[1];
+      const code = args.message.content.split('eval ')[1];
       console.log(`Eval ran: ${code}`);
-
-      const result = await eval(code);
-      if (typeof result === 'object') {
-        message.channel.send(
-          `Eval output: \n\`\`\`js\n${JSON.stringify(
-            result,
-            undefined,
-            2
-          )}\n\`\`\``
-        );
-      } else {
-        message.channel.send(`Eval output: \n\`\`\`js\n${result}\n\`\`\``);
-      }
+      if (!result) result = await eval(code);
+      console.log(`result: ${result}`);
+      sendEvalResult(args, result);
       return;
     } catch (error) {
       console.error(error);
-      message.channel
+      args.message.channel
         .send(`Error: \n\`\`\`\n${error}\n\`\`\``)
         .catch((error) => console.error(error));
     }
   },
 };
+
+function sendEvalResult(args, result) {
+  if (!result) return sendResult(args, result);
+  if (!args) return console.error('no args provided');
+
+  if (result.length >= 4000 || JSON.stringify(result).length >= 4000) {
+    if (typeof result === 'object') {
+      args.message.channel.send({
+        files: [
+          {
+            attachment: Buffer.from(JSON.stringify(result, null, 2)),
+            name: 'eval_output.txt',
+          },
+        ],
+      });
+    } else {
+      args.message.channel.send({
+        files: [{ attachment: Buffer.from(result), name: 'eval_output.txt' }],
+      });
+    }
+  } else {
+    sendResult(args, result);
+  }
+}
+
+function sendResult(args, result) {
+  if (typeof result === 'object') {
+    args.message.channel.send(
+      `Eval output: \n\`\`\`js\n${JSON.stringify(result, undefined, 2)}\n\`\`\``
+    );
+  } else {
+    args.message.channel.send(`Eval output: \n\`\`\`js\n${result}\n\`\`\``);
+  }
+}
