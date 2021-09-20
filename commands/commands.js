e1ced713f9cee582bc47528c95cd2cd68d4d1431
Zevell <zevalgaming@gmail.com>
2021-09-20 10:36:53 +1000

    Update commands.js
    list all commands names and descriptions, each being on a new line.

--- commands/commands.js
+++ commands/commands.js
@@ -1,8 +1,25 @@
 module.exports = {
-  name: 'Commands',
+  name: 'commands',
   description: 'Displays a list of commands the bot has.',
-  usage: 'commands',
-  execute(message, args, client) {
-    // code
+  usage: `commands`,
+  execute: async (args) => {
+    let commands = '\n';
+
+    if (args.commands) {
+      args.commands.forEach((command) => {
+        if (!command || !command.name) return;
+
+        commands += `${command.name}  **|**  **${command.description}**\n`;
+      });
+
+      commands += '\nFor more information on a command, use `help <command>`';
+
+      const embed = {
+        title: this.name,
+        description: `${commands}`,
+        color: args.color,
+      };
+      args.message.reply({ embed: embed });
+    }
   },
 };
